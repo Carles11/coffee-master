c08b5fea1c725dec3f9ecf222259c3bce86fab85
carlos <c.delriofrances@gmail.com>
2025-01-24 16:05:13 -0300

    added services

---
+++ app.js
@@ -0,0 +1,11 @@
+
+import Store from "./services/Store.js"
+import { loadData } from "./services/Menu.js"
+
+window.app = {}
+app.store = Store
+
+// It´s better to wait for the event for manipulation
+window.addEventListener("DOMContentLoaded", async ()=>{
+   loadData()
+})
---
+++ services/Menu.js
@@ -0,0 +1,7 @@
+import API from "./API.js";
+
+
+export async function loadData(){
+    const getMenu = await API.fetchMenu()
+    app.store.menu = getMenu
+}
